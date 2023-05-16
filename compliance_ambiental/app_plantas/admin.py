33a28a09449df00b1e4ea1cc89104f8cdd958dd8
MikeInhotaS <inhotam@gmail.com>
2023-05-16 00:11:13 -0300

    Quarto - Plantas no BD e det_plantas estilizado.

--- compliance_ambiental/app_plantas/admin.py
+++ compliance_ambiental/app_plantas/admin.py
@@ -4,3 +4,4 @@ from django.contrib import admin
 from .models import Plantas, Projetos
 
 admin.site.register(Plantas)
+admin.site.register(Projetos)
--- compliance_ambiental/app_plantas/urls.py
+++ compliance_ambiental/app_plantas/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login_app, name='login_app'),
     path('plataforma/', views.plataforma, name='plataforma'),
-    path('plantas/', views.plantas, name='plantas')
+    path('plantas/', views.plantas, name='plantas'),
+    path('detalhe_planta/<int:id_planta>', views.detalhe_planta, name='detalhe_planta')
 ]
